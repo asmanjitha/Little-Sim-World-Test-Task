c56bd1db0969ed9d05a776385e95cc82bc1cf44c
asmanjitha <manjitha.15@cse.mrt.ac.lk>
2021-06-16 23:01:48 +0530

    first commit

---
+++ Assets/Scripts/Game Scene/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D playerRigidBody;
+    public Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        playerRigidBody.MovePosition(playerRigidBody.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+
+}
